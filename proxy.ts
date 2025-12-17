c108e55c08a8c03576ccca0eabddb233faf01aef
shysssthanhtri <shyssthanhtri@gmail.com>
2025-12-17 18:06:15 +0700

    feat: add prisma

--- proxy.ts
+++ proxy.ts
@@ -20,5 +20,5 @@ export default auth(async (request) => {
 
 // Optionally, don't invoke Middleware on some paths
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api|_next/static|images|_next/image|favicon.ico).*)"],
 };
