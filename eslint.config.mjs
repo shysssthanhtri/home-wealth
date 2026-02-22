3a1981852b035c0fa4f281fa30c69cbe30cd7839
tringuyen-theiconic <tri.nguyen@theiconic.com.au>
2026-02-22 12:04:38 +0700

    feat: merge with feature branch

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -37,6 +37,8 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    // Auto-generated files:
+    "generated/**",
   ]),
 ]);
 
---
+++ types/index.ts
@@ -0,0 +1,3 @@
+export type TransactionType = "income" | "expense";
+
+export type MemberRole = "admin" | "member";
